0ef8a7042c40c8c6caaf8f93bd773c7c60a6d6eb
hiteshchopra11 <34310832+hiteshchopra11@users.noreply.github.com>
2017-12-27 16:12:34 +0530

    Add files via upload

---
+++ check_ph_number.py
@@ -0,0 +1,21 @@
+import re
+
+#Function to check if the phone number is a valid Indian Number or not
+def phone_numbers(ph_number):
+    regex=re.compile('^((\+)?91)(\ )?(\d{10})$',re.VERBOSE)
+    ph=regex.search(ph_number)
+    if ph is None:
+        print("No, "+ph.group()+ " is not a valid Indian Ph. number")
+    else:
+        print("Yes, "+ph.group()+" is a valid Indian Ph. number")
+
+
+#Input Phone number
+response =input("Please enter your phone number \n")
+
+#Call the function "phone_numbers"
+phone_numbers(response)
+
+
+
+

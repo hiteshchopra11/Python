991ad90efda6e1c44466071c70c21b4a9095c8c8
hiteshchopra11 <34310832+hiteshchopra11@users.noreply.github.com>
2017-12-27 17:33:29 +0530

    Update check_ph_number.py

--- check_ph_number.py
+++ check_ph_number.py
@@ -1,20 +1,23 @@
 import re
 
+import sys
+if len(sys.argv) < 2:
+    print('Usage: python check_ph_number.py [phone-number]')
+    sys.exit()
+
+ph_number = sys.argv[1]
+
 #Function to check if the phone number is a valid Indian Number or not
 def phone_numbers(ph_number):
     regex=re.compile('^((\+)?91)(\ )?(\d{10})$',re.VERBOSE)
     ph=regex.search(ph_number)
     if ph is None:
-        print("No, "+ph_number+" is not a valid Indian Ph. number")
+        print("No, " +ph_number+" is not a valid Indian Ph. number")
     else:
-        print("Yes, "+ph_number +" is a valid Indian Ph. number")
+        print("Yes, "+ph_number+" is a valid Indian Ph. number")
 
 
-#Input Phone number
-response =input("Please enter your phone number \n")
-
-#Call the function "phone_numbers"
-phone_numbers((str(response))
+phone_numbers(ph_number)
 
 
 

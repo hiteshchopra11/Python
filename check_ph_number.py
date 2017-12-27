d0eba171f51bef814a5a9031a4e7406e99ec7824
hiteshchopra11 <34310832+hiteshchopra11@users.noreply.github.com>
2017-12-27 17:02:06 +0530

    Update check_ph_number.py

--- check_ph_number.py
+++ check_ph_number.py
@@ -5,16 +5,16 @@ def phone_numbers(ph_number):
     regex=re.compile('^((\+)?91)(\ )?(\d{10})$',re.VERBOSE)
     ph=regex.search(ph_number)
     if ph is None:
-        print("No, "+ph.group()+ " is not a valid Indian Ph. number")
+        print("No, "+ph_number+" is not a valid Indian Ph. number")
     else:
-        print("Yes, "+ph.group()+" is a valid Indian Ph. number")
+        print("Yes, "+ph_number +" is a valid Indian Ph. number")
 
 
 #Input Phone number
 response =input("Please enter your phone number \n")
 
 #Call the function "phone_numbers"
-phone_numbers(response)
+phone_numbers((str(response))
 
 
 

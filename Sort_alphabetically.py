4bc5d6512e566277da8a0b9e7f21b57d73b6f610
hiteshchopra11 <34310832+hiteshchopra11@users.noreply.github.com>
2017-12-28 18:57:34 +0530

    Add files via upload

---
+++ Sort_alphabetically.py
@@ -0,0 +1,31 @@
+import re
+
+op=raw_input("Enter the filename\n")
+fileopen=open(op,"r")
+newfile=open('sorted.txt',"w")
+lines= fileopen.readlines()
+c=0
+for line in lines:
+    c=c+1
+    blank= re.sub("\d+\.\ ?","",line)  
+    if blank.startswith(" "):
+        blank=blank.replace(" ","")
+    newfile.write(blank)
+    #print blank
+newfile.close()
+
+my_file=open("sorted.txt","r")
+lines=my_file.readlines()
+lines.sort()
+my_file.close()
+my_file=open("sorted.txt","w")
+c=0
+for line in lines:
+    c=c+1
+    print('%d. %s')%(c,line)
+    final='%d. %s'%(c,line) 
+    my_file.write(final)
+my_file.close()
+   
+
+
